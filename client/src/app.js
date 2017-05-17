807f7b08f79c0d1495aa88168cb1beacf861e3be
Colin Farquhar <cifarquhar@gmail.com>
2017-05-17 18:12:22 +0100

    add Property model, refactor app to use it to generate properties

--- client/src/app.js
+++ client/src/app.js
@@ -3,411 +3,53 @@ import ReactDOM from 'react-dom';
 import GameContainer from "./containers/GameContainer"
 import Player from "./models/Player"
 import Card from "./models/Card"
+import Property from "./models/Property"
 
 const player1 = new Player("P1")
 const player2 = new Player("P2")
 
 const players = [player1,player2]
 
-const property1 ={
-  name: "Go",
-  owner: null,
-  group: "corner",
-  value: null,
-  rentIndex: 0,
-  rentValues: [null],
-  housePrice: null
-}
-
-const property2 ={
-  name: "Old Kent Road",
-  owner: null,
-  group: "brown",
-  value: 60,
-  rentIndex: 0,
-  rentValues: [2,4,10,30,90,160,250],
-  housePrice: 50
-}
-
-const property3 ={
-  name: "Community Chest",
-  owner: null,
-  group: "bonus",
-  value: null,
-  rentIndex: 0,
-  rentValues: [null],
-  housePrice: null
-}
-
-const property4 ={
-  name: "Whitechapel Road",
-  owner: null,
-  group: "brown",
-  value: 60,
-  rentIndex: 0,
-  rentValues: [4,8,20,60,180,320,450],
-  housePrice: 50
-}
-
-const property5 ={
-  name: "Income Tax",
-  owner: null,
-  group: "tax",
-  value: null,
-  rentIndex: 0,
-  rentValues: [200],
-  housePrice: null
-}
-
-const property6 ={
-  name: "Kings Cross Station",
-  owner: null,
-  group: "station",
-  value: 200,
-  rentIndex: 0,
-  rentValues: [25,50,100,200],
-  housePrice: null
-}
-
-const property7 ={
-  name: "The Angel, Islington",
-  owner: null,
-  group: "sky_blue",
-  value: 100,
-  rentIndex: 0,
-  rentValues: [6,12,30,90,270,400,550],
-  housePrice: 50
-}
-
-const property8 ={
-  name: "Chance",
-  owner: null,
-  group: "bonus",
-  value: null,
-  rentIndex: 0,
-  rentValues: [null],
-  housePrice: null
-}
-
-const property9 ={
-  name: "Euston Road",
-  owner: null,
-  group: "sky_blue",
-  value: 100,  
-  rentIndex: 0,
-  rentValues: [6,12,30,90,270,400,550],
-  housePrice: 50
-}
-
-const property10 ={
-  name: "Pentonville Road",
-  owner: null,
-  group: "sky_blue",
-  value: 120,  
-  rentIndex: 0,
-  rentValues: [8,16,40,100,300,450,600],
-  housePrice: 50
-}
-
-const property11 ={
-  name: "Jail",
-  owner: null,
-  group: "corner",
-  value: null,  
-  rentIndex: 0,
-  rentValues: [null],
-  housePrice: null
-}
-
-const property12 ={
-  name: "Pall Mall",
-  owner: null,
-  group: "pink",
-  value: 140,  
-  rentIndex: 0,
-  rentValues: [10,20,50,150,450,625,750],
-  housePrice: 100
-}
-
-const property13 ={
-  name: "Electric Company",
-  owner: null,
-  group: "utility",
-  value: 150,  
-  rentIndex: 0,
-  rentValues: [0,4,10],
-  housePrice: null
-}
-
-const property14 ={
-  name: "Whitehall",
-  owner: null,
-  group: "pink",
-  value: 140,  
-  rentIndex: 0,
-  rentValues: [10,20,50,150,450,625,750],
-  housePrice: 100
-}
-
-const property15 ={
-  name: "Northumberland Avenue",
-  owner: null,
-  group: "pink",
-  value: 160,  
-  rentIndex: 0,
-  rentValues: [12,24,60,180,500,700,900],
-  housePrice: 100
-}
-
-const property16 ={
-  name: "Marylebone Station",
-  owner: null,
-  group: "station",
-  value: 200,  
-  rentIndex: 0,
-  rentValues: [25,50,100,200],
-  housePrice: null
-}
-
-const property17 ={
-  name: "Bow Street",
-  owner: null,
-  group: "orange",
-  value: 180,  
-  rentIndex: 0,
-  rentValues: [14,28,70,200,550,750,950],
-  housePrice: 100
-}
-
-const property18 ={
-  name: "Community Chest",
-  owner: null,
-  group: "bonus",
-  value: null,  
-  rentIndex: 0,
-  rentValues: [null],
-  housePrice: null
-}
-
-const property19 ={
-  name: "Marlborough Street",
-  owner: null,
-  group: "orange",
-  value: 180,  
-  rentIndex: 0,
-  rentValues: [14,28,70,200,550,750,950],
-  housePrice: 100
-}
-
-const property20 ={
-  name: "Vine Street",
-  owner: null,
-  group: "orange",
-  value: 200,  
-  rentIndex: 0,
-  rentValues: [16,32,80,220,600,800,1000],
-  housePrice: 100
-}
-
-const property21 ={
-  name: "Free Parking",
-  owner: null,
-  group: "corner",
-  value: null,  
-  rentIndex: 0,
-  rentValues: [null],
-  housePrice: null
-}
-
-const property22 ={
-  name: "Strand",
-  owner: null,
-  group: "red",
-  value: 220,  
-  rentIndex: 0,
-  rentValues: [18,36,90,250,700,875,1050],
-  housePrice: 150
-}
-
-const property23 ={
-  name: "Chance",
-  owner: null,
-  group: "bonus",
-  value: null,
-  rentIndex: 0,
-  rentValues: [null],
-  housePrice: null
-}
-
-const property24 ={
-  name: "Fleet Street",
-  owner: null,
-  group: "red",
-  value: 220,
-  rentIndex: 0,
-  rentValues: [18,36,90,250,700,875,1050],
-  housePrice: 150
-}
-
-const property25 ={
-  name: "Trafalgar Square",
-  owner: null,
-  group: "red",
-  value: 240,
-  rentIndex: 0,
-  rentValues: [20,40,100,300,750,925,1100],
-  housePrice: 150
-}
-
-const property26 ={
-  name: "Fenchurch St Station",
-  owner: null,
-  group: "station",
-  value: 200,
-  rentIndex: 0,
-  rentValues: [25,50,100,200],
-  housePrice: null
-}
-
-const property27 ={
-  name: "Leicester Square",
-  owner: null,
-  group: "yellow",
-  value: 260,
-  rentIndex: 0,
-  rentValues: [22,44,110,330,800,975,1150],
-  housePrice: 150
-}
-
-const property28 ={
-  name: "Coventry Street",
-  owner: null,
-  group: "yellow",
-  value: 260,
-  rentIndex: 0,
-  rentValues: [22,44,110,330,800,975,1150],
-  housePrice: 150
-}
-
-const property29 ={
-  name: "Water Works",
-  owner: null,
-  group: "utility",
-  value: 150,
-  rentIndex: 0,
-  rentValues: [0,4,10],
-  housePrice: null
-}
-
-const property30 ={
-  name: "Picadilly",
-  owner: null,
-  group: "yellow",
-  value: 280,
-  rentIndex: 0,
-  rentValues: [22,44,120,360,850,1025,1200],
-  housePrice: 150
-}
-
-const property31 ={
-  name: "Go To Jail",
-  owner: null,
-  group: "corner",
-  value: null,
-  rentIndex: 0,
-  rentValues: [null],
-  housePrice: null
-}
-
-const property32 ={
-  name: "Regent Street",
-  owner: null,
-  group: "green",
-  value: 300,
-  rentIndex: 0,
-  rentValues: [26,52,130,390,900,1100,1275],
-  housePrice: 200
-}
-
-const property33 ={
-  name: "Oxford Street",
-  owner: null,
-  group: "green",
-  value: 300,
-  rentIndex: 0,
-  rentValues: [26,52,130,390,900,1100,1275],
-  housePrice: 200
-}
-
-const property34 ={
-  name: "Community Chest",
-  owner: null,
-  group: "bonus",
-  value: null,
-  rentIndex: 0,
-  rentValues: [null],
-  housePrice: null
-}
-
-const property35 ={
-  name: "Bond Street",
-  owner: null,
-  group: "green",
-  value: 320,
-  rentIndex: 0,
-  rentValues: [28,56,150,450,1000,1200,1400],
-  housePrice: 200
-}
-
-const property36 ={
-  name: "Liverpool St Station",
-  owner: null,
-  group: "station",
-  value: 200,
-  rentIndex: 0,
-  rentValues: [25,50,100,200],
-  housePrice: null
-}
-
-const property37 ={
-  name: "Chance",
-  owner: null,
-  group: "bonus",
-  value: null,
-  rentIndex: 0,
-  rentValues: [null],
-  housePrice: null
-}
-
-const property38 ={
-  name: "Park Lane",
-  owner: null,
-  group: "dark_blue",
-  value: 350,
-  rentIndex: 0,
-  rentValues: [35,70,175,500,1100,1300,1500],
-  housePrice: 200
-}
-
-const property39 ={
-  name: "Super Tax",
-  owner: null,
-  group: "tax",
-  value: null,
-  rentIndex: 0,
-  rentValues: [100],
-  housePrice: null
-}
-
-const property40 ={
-  name: "Mayfair",
-  owner: null,
-  group: "dark_blue",
-  value: 400,
-  rentIndex: 0,
-  rentValues: [50,100,200,600,1400,1700,2000],
-  housePrice: 200
-}
+const property1 = new Property("Go","corner",null,[null],null)
+const property2 = new Property("Old Kent Road","brown",60,[2,4,10,30,90,160,250],50)
+const property3 = new Property("Community Chest","bonus",null,[null],null)
+const property4 = new Property("Whitechapel Road","brown",60,[4,8,20,60,180,320,450],50)
+const property5 = new Property("Income Tax","tax",null,[200],null)
+const property6 = new Property("Kings Cross Station","station",200,[25,50,100,200],null)
+const property7 = new Property("The Angel, Islington","sky_blue",100,[6,12,30,90,270,400,550],50)
+const property8 = new Property("Chance","bonus",null,[null],null)
+const property9 = new Property("Euston Road","sky_blue",100,[6,12,30,90,270,400,550],50)
+const property10 = new Property("Pentonville Road","sky_blue",120,[8,16,40,100,300,450,600],50)
+const property11 = new Property("Jail","corner",null,[null],null)
+const property12 = new Property("Pall Mall","pink",140,[10,20,50,150,450,625,750],100)
+const property13 = new Property("Electric Company","utility",150,[0,4,10],null)
+const property14 = new Property("Whitehall","pink",140,[10,20,50,150,450,625,750],100)
+const property15 = new Property("Northumberland Avenue","pink",160,[12,24,60,180,500,700,900],100)
+const property16 = new Property("Marylebone Station","station",200,[25,50,100,200],null)
+const property17 = new Property("Bow Street","orange",180,[14,28,70,200,550,750,950],100)
+const property18 = new Property("Community Chest","bonus",null,[null],null)
+const property19 = new Property("Marlborough Street","orange",180,[14,28,70,200,550,750,950],100)
+const property20 = new Property("Vine Street","orange",200,[16,32,80,220,600,800,1000],100)
+const property21 = new Property("Free Parking","corner",null,[null],null)
+const property22 = new Property("Strand","red",220,[18,36,90,250,700,875,1050],150)
+const property23 = new Property("Chance","bonus",null,[null],null)
+const property24 = new Property("Fleet Street","red",220,[18,36,90,250,700,875,1050],150)
+const property25 = new Property("Trafalgar Square","red",240,[20,40,100,300,750,925,1100],150)
+const property26 = new Property("Fenchurch St Station","station",200,[25,50,100,200],null)
+const property27 = new Property("Leicester Square","yellow",260,[22,44,110,330,800,975,1150],150)
+const property28 = new Property("Coventry Street","yellow",260,[22,44,110,330,800,975,1150],150)
+const property29 = new Property("Water Works","utility",150,[0,4,10],null)
+const property30 = new Property("Picadilly","yellow",280,[22,44,120,360,850,1025,1200],150)
+const property31 = new Property("Go To Jail","corner",null,[null],null)
+const property32 = new Property("Regent Street","green",300,[26,52,130,390,900,1100,1275],200)
+const property33 = new Property("Oxford Street","green",300,[26,52,130,390,900,1100,1275],200)
+const property34 = new Property("Community Chest","bonus",null,[null],null)
+const property35 = new Property("Bond Street","green",320,[28,56,150,450,1000,1200,1400],200)
+const property36 = new Property("Liverpool St Station","station",200,[25,50,100,200],null)
+const property37 = new Property("Chance","bonus",null,[null],null)
+const property38 = new Property("Park Lane","dark_blue",350,[35,70,175,500,1100,1300,1500],200)
+const property39 = new Property("Super Tax","tax",null,[100],null)
+const property40 = new Property("Mayfair","dark_blue",400,[50,100,200,600,1400,1700,2000],200)
 
 const properties = [property1,property2,property3,property4,property5,property6,property7,property8,property9,property10,property11,property12,property13,property14,property15,property16,property17,property18,property19,property20,property21,property22,property23,property24,property25,property26,property27,property28,property29,property30,property31,property32,property33,property34,property35,property36,property37,property38,property39,property40]
 
