779b0e997020b16d50c72f80e9cbd57091be449f
Colin Farquhar <cifarquhar@gmail.com>
2017-11-05 12:58:28 +0000

    refactor board to use bootstrap grid, now actually looks like a monopoly board

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import Square from "./Square"
+import FullRow from "./FullRow"
+import {Grid, Row, Col} from "react-bootstrap"
 
 class Board extends React.Component{
 
@@ -42,11 +44,78 @@ class Board extends React.Component{
       )
   })
 
+  // return(
+  //   <div className="board">
+  //   {squareNodes}
+  //   </div>
+  //   )
+
   return(
-    <div className="board">
-    {squareNodes}
-    </div>
-    )
+    <Grid>
+      <Row>
+        <Col md={1}>{squareNodes[0]}</Col>
+        <Col md={1}>{squareNodes[1]}</Col>
+        <Col md={1}>{squareNodes[2]}</Col>
+        <Col md={1}>{squareNodes[3]}</Col>
+        <Col md={1}>{squareNodes[4]}</Col>
+        <Col md={1}>{squareNodes[5]}</Col>
+        <Col md={1}>{squareNodes[6]}</Col>
+        <Col md={1}>{squareNodes[7]}</Col>
+        <Col md={1}>{squareNodes[8]}</Col>
+        <Col md={1}>{squareNodes[9]}</Col> 
+        <Col md={1}>{squareNodes[10]}</Col>
+      </Row>
+      <Row>
+        <Col md={1}>{squareNodes[39]}</Col>
+        <Col md={1} mdOffset={9}>{squareNodes[11]}</Col>
+      </Row>      
+      <Row>
+        <Col md={1}>{squareNodes[38]}</Col>
+        <Col md={1} mdOffset={9}>{squareNodes[12]}</Col>
+      </Row>      
+      <Row>
+        <Col md={1}>{squareNodes[37]}</Col>
+        <Col md={1} mdOffset={9}>{squareNodes[13]}</Col>
+      </Row>      
+      <Row>
+        <Col md={1}>{squareNodes[36]}</Col>
+        <Col md={1} mdOffset={9}>{squareNodes[14]}</Col>
+      </Row>      
+      <Row>
+        <Col md={1}>{squareNodes[35]}</Col>
+        <Col md={1} mdOffset={9}>{squareNodes[15]}</Col>
+      </Row>      
+      <Row>
+        <Col md={1}>{squareNodes[34]}</Col>
+        <Col md={1} mdOffset={9}>{squareNodes[16]}</Col>
+      </Row>      
+      <Row>
+        <Col md={1}>{squareNodes[33]}</Col>
+        <Col md={1} mdOffset={9}>{squareNodes[17]}</Col>
+      </Row>      
+      <Row>
+        <Col md={1}>{squareNodes[32]}</Col>
+        <Col md={1} mdOffset={9}>{squareNodes[18]}</Col>
+      </Row>      
+      <Row>
+        <Col md={1}>{squareNodes[31]}</Col>
+        <Col md={1} mdOffset={9}>{squareNodes[19]}</Col>
+      </Row>
+      <Row>
+        <Col md={1}>{squareNodes[20]}</Col>
+        <Col md={1}>{squareNodes[21]}</Col>
+        <Col md={1}>{squareNodes[22]}</Col>
+        <Col md={1}>{squareNodes[23]}</Col>
+        <Col md={1}>{squareNodes[24]}</Col>
+        <Col md={1}>{squareNodes[25]}</Col>
+        <Col md={1}>{squareNodes[26]}</Col>
+        <Col md={1}>{squareNodes[27]}</Col>
+        <Col md={1}>{squareNodes[28]}</Col>
+        <Col md={1}>{squareNodes[29]}</Col>
+        <Col md={1}>{squareNodes[30]}</Col>
+      </Row>
+    </Grid>
+  )
 }
 
 }
