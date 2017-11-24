9df2348ac1be3031d721b4d6dd3a8f9f992f9de7
Colin Farquhar <cifarquhar@gmail.com>
2017-11-24 14:30:14 +0000

    add group colour divs, needs more work

--- client/src/components/Square.jsx
+++ client/src/components/Square.jsx
@@ -9,25 +9,36 @@ class Square extends React.Component{
 
   getClassName(index){
     if ([0,10,20,30].includes(index)) {
-      return "box corner"
+      return "corner"
     }
-    else if ([1,2,3,4,5,6,7,8,9,21,22,23,24,25,26,27,28,29].includes(index)) {
-      return "box top"
+    else if ([1,2,3,4,5,6,7,8,9].includes(index)) {
+      return "top"
     }
-    else {
-      return "box side"
+    else if ([21,22,23,24,25,26,27,28,29].includes(index)) {
+      return "bottom"
+    }
+    else if ([11,12,13,14,15,16,17,18,19].includes(index)) {
+      return "right"
+    }
+    else if ([31,32,33,34,35,36,37,38,39].includes(index)) {
+      return "left"
     }
   }
 
   render(){
+    const boardSide = this.getClassName(this.props.index)
+
     return(
-      <div className={this.getClassName(this.props.index)}>
-        <p>{this.props.value.name}</p>
-        <p>{this.props.players ? this.props.players.map((player) => {
-                                                        return player.name
-                                                        }).toString() : null}</p>
+      <div className={"box " + boardSide}>
+        <div className={"groupDiv " + boardSide} style={{backgroundColor: "red"}} />
+        <div>
+          <p>{this.props.value.name}</p>
+          <p>{this.props.players ? this.props.players.map((player) => {
+              return player.name
+                }).toString() : null}</p>
+        </div>
       </div>
-    )
+      )
   }
 
 }
