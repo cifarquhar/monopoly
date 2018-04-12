7f5cfac05b1e9ace89e4f12036b8a4d1876cc72d
Colin Farquhar <cifarquhar@gmail.com>
2018-04-12 16:08:20 +0100

    fix typos

--- client/src/components/Square.jsx
+++ client/src/components/Square.jsx
@@ -12,16 +12,16 @@ class Square extends React.Component{
       return "corner"
     }
     else if ([1,2,3,4,5,6,7,8,9].includes(index) && !["bonus", "tax", "station", "utility"].includes(group)) {
-      return " top"
+      return "top"
     }
     else if ([21,22,23,24,25,26,27,28,29].includes(index) && !["bonus", "tax", "station", "utility"].includes(group)) {
-      return " bottom"
+      return "bottom"
     }
     else if ([11,12,13,14,15,16,17,18,19].includes(index) && !["bonus", "tax", "station", "utility"].includes(group)) {
-      return " right"
+      return "right"
     }
     else if ([31,32,33,34,35,36,37,38,39].includes(index) && !["bonus", "tax", "station", "utility"].includes(group)) {
-      return " left"
+      return "left"
     }
     else {
       return ""
@@ -36,7 +36,7 @@ class Square extends React.Component{
 
     return(
       <div className={"box outer " + boardSide}>
-        <div className={"groupDiv" + markerSide + " " + p.value.group} />
+        <div className={"groupDiv " + markerSide + " " + p.value.group} />
         <div>
           <p><strong>{p.value.name}</strong></p>
           <p>{p.players ? p.players.map((player) => {
